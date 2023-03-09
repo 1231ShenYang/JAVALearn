055730ec3167a2b794e188c7f0961500690b1d63
yxy <ys02@Y-TAISHI>
2023-03-09 19:00:49 +0800

    Initial commit

---
+++ src/main/java/TEST.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TEST
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String str1 = input.nextLine();
+        System.out.println(str1);
+    }
+}
