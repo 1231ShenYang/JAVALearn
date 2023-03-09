e44a5c4550e5aca2ded6c2dd011f362c63dd1ebf
yxy <ys02@Y-TAISHI>
2023-03-09 19:11:13 +0800

    1.2

---
+++ src/main/java/S1.java
@@ -0,0 +1,18 @@
+public class S1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is the titles of Section 1");
+        q2();
+    }
+    static void q1()
+    {
+        System.out.println("Welcome to java.Welcom to computer Science");
+        System.out.println("Programming is fun");
+    }
+    static void q2()
+    {
+        for(int a = 0; a < 5; ++a) System.out.println("Welcome to java");
+    }
+
+}
